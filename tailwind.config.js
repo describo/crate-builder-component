0876d448c93ff65c1a81a3e8a3d74066bc6de465
Marco La Rosa <m@lr.id.au>
2022-09-15 10:19:58 +1000

    check in plugin development requirements

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    future: {},
+    mode: "jit",
+    content: ["./src/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+};
---
+++ webpack-common.js
@@ -0,0 +1,90 @@
+"use strict";
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { VueLoaderPlugin } = require("vue-loader");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    target: "web",
+    entry: ["./src/app/main.js"],
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[contenthash].js",
+        publicPath: "http://localhost:9000/",
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: "Describo Components",
+            template: "./index.html",
+        }),
+        new VueLoaderPlugin(),
+        new MiniCssExtractPlugin({ filename: "[contenthash].css" }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: "vue-loader",
+            },
+            {
+                test: /\.mjs$/,
+                resolve: {
+                    fullySpecified: false,
+                },
+                include: /node_modules/,
+                type: "javascript/auto",
+            },
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: "",
+                        },
+                    },
+                    "css-loader",
+                    "postcss-loader",
+                ],
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: "",
+                        },
+                    },
+                    "css-loader",
+                    "sass-loader",
+                    "postcss-loader",
+                ],
+            },
+            {
+                test: /\.(svg|png|jp(e*)g|gif|mp4)?$/,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                type: "asset/resource",
+            },
+        ],
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"),
+            src: path.resolve(__dirname, "src"),
+            assets: path.resolve(__dirname, "src/assets"),
+            components: path.resolve(__dirname, "src/components"),
+        },
+    },
+};
