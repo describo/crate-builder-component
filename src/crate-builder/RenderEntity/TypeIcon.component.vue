5e79860f2b6d1aaf0dcb017f0cfcffe987acf9b7
Marco La Rosa <m@lr.id.au>
2022-12-16 14:12:36 +1100

    revise as composition

--- src/crate-builder/RenderEntity/TypeIcon.component.vue
+++ src/crate-builder/RenderEntity/TypeIcon.component.vue
@@ -1,46 +1,44 @@
 <template>
     <div>
-        <i class="fa-solid fa-fw" :class="types[type]" v-if="types[type]"></i>
+        <i class="fa-solid fa-fw" :class="data.types[props.type]" v-if="data.types[props.type]"></i>
     </div>
 </template>
 
-<script>
-export default {
-    props: {
-        type: {
-            type: String,
-            required: true,
-        },
+<script setup>
+import { reactive } from "vue";
+const props = defineProps({
+    type: {
+        type: String,
+        required: true,
     },
-    data() {
-        return {
-            types: {
-                ContactPoint: "fa-address-card",
-                CreateAction: "fa-plus",
-                CreativeWork: "fa-image",
-                Country: "fa-flag",
-                Dataset: "fa-folder",
-                DataDownload: "fa-download",
-                File: "fa-file",
-                GeoCoordinates: "fa-map-marker-alt",
-                GeoShape: "fa-map-marker-alt",
-                IndividualProduct: "fa-barcode",
-                Language: "fa-sign-language",
-                Organization: "fa-university",
-                Organisation: "fa-university",
-                Person: "fa-user",
-                Place: "fa-globe",
-                PropertyValue: "fa-paragraph",
-                Publication: "fa-newspaper",
-                RepositoryCollection: "fa-cubes",
-                RepositoryObject: "fa-cube",
-                ScholarlyArticle: "fa-scroll",
-                Script: "fa-code",
-                SoftwareApplication: "fa-code",
-                Workflow: "fa-project-diagram",
-                WorkflowSketch: "fa-project-diagram",
-            },
-        };
+});
+
+const data = reactive({
+    types: {
+        ContactPoint: "fa-address-card",
+        CreateAction: "fa-plus",
+        CreativeWork: "fa-image",
+        Country: "fa-flag",
+        Dataset: "fa-folder",
+        DataDownload: "fa-download",
+        File: "fa-file",
+        GeoCoordinates: "fa-map-marker-alt",
+        GeoShape: "fa-map-marker-alt",
+        IndividualProduct: "fa-barcode",
+        Language: "fa-sign-language",
+        Organization: "fa-university",
+        Organisation: "fa-university",
+        Person: "fa-user",
+        Place: "fa-globe",
+        PropertyValue: "fa-paragraph",
+        Publication: "fa-newspaper",
+        RepositoryCollection: "fa-cubes",
+        RepositoryObject: "fa-cube",
+        ScholarlyArticle: "fa-scroll",
+        Script: "fa-code",
+        SoftwareApplication: "fa-code",
+        Workflow: "fa-project-diagram",
+        WorkflowSketch: "fa-project-diagram",
     },
-};
+});
 </script>
