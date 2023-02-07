2a824f186cf7f82076c07fe6cd39c9833f46ddfa
Marco La Rosa <m@lr.id.au>
2023-02-08 08:13:14 +1100

    deprecate webpack dev env; setup vite based env

---
+++ vite.wc.config.js
@@ -0,0 +1,27 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+const path = require("path");
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    build: {
+        outDir: "./dist/web-component",
+        lib: {
+            entry: "./src/crate-builder/index.wc.js",
+            name: "DescriboCrateBuilderWC",
+            fileName: "describo-crate-builder-wc",
+        },
+        // Don't minify so there's a chance to debug problems while experimenting
+        minify: false,
+        minifySyntax: false,
+    },
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
+});
