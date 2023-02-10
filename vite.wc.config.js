e269c5b9717dfa5150ec05d47bc37c311165c7d5
Marco La Rosa <m@lr.id.au>
2023-02-11 10:16:12 +1100

    update vite config with element plus resolvers

--- vite.wc.config.js
+++ vite.wc.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
-const path = require("path");
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,5 +17,14 @@ export default defineConfig({
         minify: false,
         minifySyntax: false,
     },
-    plugins: [vue()],
+    plugins: [
+        vue({}),
+        // https://element-plus.org/en-US/guide/quickstart.html#on-demand-import
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()],
+        }),
+    ],
 });
