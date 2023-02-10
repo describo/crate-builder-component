901e635c1be8b4cca32a1de976b172d9f7ed3988
Marco La Rosa <m@lr.id.au>
2023-02-11 10:15:07 +1100

    split out css includes into own file so we can pre bundle them
    If we don't split it out the rollup config becomes more complex as we
    need to handle the vue code as well. And since this step is about
    bundling the css for use in the vue code later, we just split it out on
    its own.

--- src/crate-builder/index.wc.js
+++ src/crate-builder/index.wc.js
@@ -1,19 +1,9 @@
 import { defineCustomElement } from "vue";
 import DescriboCrateBuilder from "./Shell.component.wc.vue";
-import "./tailwind.css";
-import "@element-plus/theme-chalk/dist/index.css";
-import "./component.css";
-import ElementPlus from "element-plus";
-import "@fortawesome/fontawesome-free/js/all";
-import { config } from "@fortawesome/fontawesome-svg-core";
-config.autoReplaceSvg = "nest";
 
-// The trick is to import from the vue build, because it is built before this, so style.css is already available.
-// If we tried to import from dist/web-component/style.css it was not available since we are in the building process
-// and so style.css wouldbe only available at the end of it.
-import css from "../../dist/vue/style.css";
-
-// Explicitly inline styles in the component
+// We need to import and inline the styles into the component. These
+//   have been built by an earlier rollup process.
+import css from "../../dist/web-component-styles/styles.css";
 DescriboCrateBuilder.styles = [css];
 
 // Web Component build entrypoint
