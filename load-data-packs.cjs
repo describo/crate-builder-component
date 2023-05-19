66a22e581c22802afad702617f3dc80ac3cac57f
Marco La Rosa <m@lr.id.au>
2023-05-19 11:09:12 +1000

    add elastic client and create some fake entity content

--- load-data-packs.cjs
+++ load-data-packs.cjs
@@ -1,7 +1,38 @@
 const { IndexDataPacks } = require("@describo/data-packs");
+const { Client } = require("@elastic/elasticsearch");
 
+const elasticUrl = "http://localhost:9200";
 main();
 async function main() {
-    const index = new IndexDataPacks({ elasticUrl: "http://localhost:9200", log: true });
+    const index = new IndexDataPacks({ elasticUrl, log: true });
     await index.load();
+
+    const client = new Client({
+        node: elasticUrl,
+    });
+
+    let entities = [
+        {
+            "@id": "#person1",
+            "@type": "Person",
+            name: "person1",
+        },
+        {
+            "@id": "#person2",
+            "@type": ["Person", "Human"],
+            name: "person2",
+        },
+        {
+            "@id": "#language1",
+            "@type": "Language",
+            name: "language1",
+        },
+    ];
+    for (let entity of entities) {
+        await client.index({
+            index: "entities",
+            id: encodeURIComponent(entity["@id"]),
+            document: entity,
+        });
+    }
 }
