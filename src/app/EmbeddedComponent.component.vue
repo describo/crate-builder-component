0af3c77bcd7e6f895ef928f54c3e4391d7359526
Marco La Rosa <m@lr.id.au>
2023-05-25 11:13:59 +1000

    implement and wire up more test cases

--- src/app/EmbeddedComponent.component.vue
+++ src/app/EmbeddedComponent.component.vue
@@ -55,6 +55,7 @@ import crateFile3 from "../examples/item/complex-item/ro-crate-metadata.json";
 import crateFile4 from "../examples/item/large-crate/ro-crate-metadata.json";
 import crateFile5 from "../examples/item/ridiculously-big-collection/ro-crate-metadata.json";
 import crateFile6 from "../examples/item/item-with-relationship-and-action/ro-crate-metadata.json";
+import crateFile7 from "../examples/item/multiple-types/ro-crate-metadata.json";
 import profile1 from "../examples/profile/test-profile-without-groups.json";
 import profile2 from "../examples/profile/test-profile-with-groups.json";
 import profile3 from "../examples/profile/test-profile-with-datapacks-and-without-groups.json";
@@ -62,6 +63,7 @@ import profile4 from "../examples/profile/nyingarn-item-profile.json";
 import profile5 from "../examples/profile/profile-with-all-primitives.json";
 import profile6 from "../examples/profile/test-profile-with-resolve.json";
 import profile7 from "../examples/profile/profile-with-all-primitives-groups-no-about.json";
+import profile8 from "../examples/profile/profile-to-test-multiple-types.json";
 const lookup = new Lookup();
 
 const data = reactive({
@@ -72,6 +74,7 @@ const data = reactive({
     },
     crates: [
         { name: "blank", value: crateFile1 },
+        { name: "Multiple Types", value: crateFile7 },
         { name: "Complex Collection", value: crateFile2 },
         { name: "Complex Item", value: crateFile3 },
         { name: "Large Crate", value: crateFile4 },
@@ -79,13 +82,14 @@ const data = reactive({
         { name: "Item with Relationship and Action", value: crateFile6 },
     ],
     profiles: [
-        { name: "Profile -groups", value: profile1 },
-        { name: "Profile +groups", value: profile2 },
-        { name: "Profile +datapacks, -groups", value: profile3 },
-        { name: "Profile Nyingarn Item", value: profile4 },
         { name: "All primitives", value: profile5 },
+        { name: "Profile with multiple types", value: profile8 },
+        { name: "Profile without groups", value: profile1 },
+        { name: "Profile with groups", value: profile2 },
+        { name: "Profile with datapacks and without groups", value: profile3 },
         { name: "Test profile with resolve", value: profile6 },
-        { name: "Primitives, bad group definition", value: profile7 },
+        { name: "Primitives and bad group definition", value: profile7 },
+        { name: "Profile Nyingarn Item", value: profile4 },
     ],
     selectedCrate: undefined,
     selectedProfile: undefined,
