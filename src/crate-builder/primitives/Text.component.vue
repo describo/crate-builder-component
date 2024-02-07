fbfe7dfa0d11297552ab95b1bdef8603916b5512
Marco La Rosa <m@lr.id.au>
2024-02-08 10:01:34 +1100

    formatting, cleanup, revise dayjs import
    Wasn't working when crate builder used as a node module

--- src/crate-builder/primitives/Text.component.vue
+++ src/crate-builder/primitives/Text.component.vue
@@ -1,8 +1,16 @@
 <template>
     <div class="flex flex-col space-y-2 describo-property-type-text">
         <div class="flex flex-row space-x-2">
-            <el-input class="w-full" :type="type" v-model="data.displayValue" @blur="debouncedSave" @change="debouncedSave"
-                resize="vertical" :rows="5" :placeholder="props.placeholder"></el-input>
+            <el-input
+                class="w-full"
+                :type="type"
+                v-model="data.displayValue"
+                @blur="debouncedSave"
+                @change="debouncedSave"
+                resize="vertical"
+                :rows="5"
+                :placeholder="props.placeholder"
+            ></el-input>
             <el-button @click="save" type="success" size="default" :disabled="!isValidTextValue">
                 <i class="fas fa-check fa-fw"></i>
             </el-button>
@@ -19,9 +27,7 @@ import { reactive, watch, computed } from "vue";
 import debounce from "lodash-es/debounce.js";
 import isBoolean from "lodash-es/isBoolean.js";
 import { $t } from "../i18n";
-import { isNavigationFailure } from "vue-router";
-import { isNaN } from "lodash";
-import dayjs from 'dayjs'
+import * as dayjs from "dayjs";
 const debouncedSave = debounce(save, 200);
 
 const props = defineProps({
@@ -50,7 +56,7 @@ const data = reactive({
         maxLength: props.definition?.maxLength,
         regex: props.definition?.regex,
         dateFormat: props.definition?.dateFormat,
-    }
+    },
 });
 watch(
     () => props.value,
@@ -78,44 +84,40 @@ function decodeValue(value) {
 }
 
 function validateTextConstraints(value) {
-    if (
-        data.constraints.minLength !== undefined &&
-        data.constraints.minLength > value.length
-    ) return false;
-    if (
-        data.constraints.maxLength !== undefined &&
-        data.constraints.maxLength < value.length
-    ) return false;
-    if (
-        data.constraints.regex !== undefined &&
-        !new RegExp(data.constraints.regex).test(value)
-    ) return false;
+    if (data.constraints.minLength !== undefined && data.constraints.minLength > value.length)
+        return false;
+    if (data.constraints.maxLength !== undefined && data.constraints.maxLength < value.length)
+        return false;
+    if (data.constraints.regex !== undefined && !new RegExp(data.constraints.regex).test(value))
+        return false;
     if (
         data.constraints.dateFormat !== undefined &&
         !validateDateFormat(value, data.constraints.dateFormat)
-    ) return false;
+    )
+        return false;
 
     return true;
 }
 
 function validateDateFormat(inputString, granularity) {
-    let dateString = inputString 
+    let dateString = inputString;
     if (inputString.startsWith("-")) {
-        dateString = dateString.substr(1)
+        dateString = dateString.substr(1);
     }
-    return Array.isArray(granularity) && granularity.some(g => dayjs(dateString, g, true).isValid());
+    return (
+        Array.isArray(granularity) && granularity.some((g) => dayjs(dateString, g, true).isValid())
+    );
 }
 
 function getConstraintsString() {
-    let message = []
-    Object.entries(data.constraints).forEach(constraint => {
-        const [name, value] = [...constraint]
+    let message = [];
+    Object.entries(data.constraints).forEach((constraint) => {
+        const [name, value] = [...constraint];
 
-        if (name, value) {
+        if ((name, value)) {
             message.push(`${name}: ${value}`);
         }
     });
-    return message.join(', ');
+    return message.join(", ");
 }
-
 </script>
