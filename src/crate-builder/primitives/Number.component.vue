bd8fa2e8e27c030e039c921be48f2ab54fd08f60
csontosreka <61116319+csontosreka@users.noreply.github.com>
2024-02-15 23:50:23 +0100

    Add save button for number type fields (#94)
    Co-authored-by: Csontos Réka Hanga <csontos.reka.hanga@sztaki.hu>

--- src/crate-builder/primitives/Number.component.vue
+++ src/crate-builder/primitives/Number.component.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="flex flex-col describo-property-type-number">
-        <el-input class="w-full" type="number" @change="save" v-model="data.internalValue" resize="vertical"></el-input>
+        <div class="flex flex-row space-x-2">
+            <el-input class="w-full" type="number" @change="debouncedSave" v-model="data.internalValue"
+                resize="vertical"></el-input>
+            <el-button @click="save" type="success" size="default" :disabled="!isValidNumber">
+                <i class="fas fa-check fa-fw"></i>
+            </el-button>
+        </div>
         <div class="text-xs text-gray-700" v-if="!isValidNumber">
             {{ $t("invalid_number_value", { value: data.internalValue }) }}
         </div>
@@ -11,10 +17,13 @@
 </template>
 
 <script setup>
-import { ElInput } from "element-plus";
+import { ElInput, ElButton } from "element-plus";
 import { reactive, watch, computed } from "vue";
 import isNumeric from "validator/es/lib/isNumeric";
+import debounce from "lodash-es/debounce.js";
 import { $t } from "../i18n";
+
+const debouncedSave = debounce(save, 200);
 
 const props = defineProps({
     property: {
@@ -73,27 +82,27 @@ function validateNumberConstraints(value) {
         data.constraints.maxValue < value
     ) return false;
     if (
-        data.constraints.numberType && 
+        data.constraints.numberType &&
         !isNumberType(value, data.constraints.numberType)
     ) return false;
     return true;
 }
 
 function isNumberType(value, numberTypes) {
-  const stringValue = String(value).toLowerCase();
+    const stringValue = String(value).toLowerCase();
 
-  const typeMap = {
-    'any': true,
-    'long': Number.isSafeInteger(value),
-    'int': Number.isInteger(value),
-    'float': stringValue.includes('.') && !Number.isNaN(parseFloat(value)),
-    'double': !Number.isNaN(parseFloat(value))
-  };
+    const typeMap = {
+        'any': true,
+        'long': Number.isSafeInteger(value),
+        'int': Number.isInteger(value),
+        'float': stringValue.includes('.') && !Number.isNaN(parseFloat(value)),
+        'double': !Number.isNaN(parseFloat(value))
+    };
 
-  return (
-    Array.isArray(numberTypes) &&
-    numberTypes.some(type => typeMap[type.toLowerCase()])
-  );
+    return (
+        Array.isArray(numberTypes) &&
+        numberTypes.some(type => typeMap[type.toLowerCase()])
+    );
 }
 
 function getConstraintsString() {
