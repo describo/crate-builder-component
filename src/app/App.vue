add62b71774f0dab732b9b882036accd42693725
Marco La Rosa <m@lr.id.au>
2024-05-27 12:48:08 +1000

    deprecate render entity bits

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,21 +1,7 @@
 <template>
-    <el-tabs v-model="data.activeTab" class="p-2">
-        <el-tab-pane label="Embedded Component" name="component">
-            <EmbeddedComponent v-if="data.activeTab === 'component'" />
-        </el-tab-pane>
-        <el-tab-pane label="Render Entity" name="entity">
-            <RenderEntityComponent v-if="data.activeTab === 'entity'" />
-        </el-tab-pane>
-    </el-tabs>
+    <EmbeddedComponent class="p-2" />
 </template>
 
 <script setup>
-import { ElTabs, ElTabPane } from "element-plus";
 import EmbeddedComponent from "./EmbeddedComponent.component.vue";
-import RenderEntityComponent from "./RenderEntity.component.vue";
-import { reactive } from "vue";
-
-const data = reactive({
-    activeTab: "component",
-});
 </script>
