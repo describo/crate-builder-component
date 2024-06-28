b5298f641b5c43bdc3678d94e528811839c1a055
Marco La Rosa <m@lr.id.au>
2024-06-28 11:25:30 +1000

    refactor

--- src/crate-builder/Shell.component.vue
+++ src/crate-builder/Shell.component.vue
@@ -34,8 +34,9 @@ import { ProfileManager } from "./CrateManager/profile-manager.js";
 import { $t, i18next } from "./i18n";
 import { useStateStore } from "./store.js";
 import { createPinia } from "pinia";
-if (!getCurrentInstance().appContext.app.config.globalProperties.$pinia) {
-    getCurrentInstance().appContext.app.use(createPinia());
+const app = getCurrentInstance().appContext.app;
+if (!app.config.globalProperties.$pinia) {
+    app.use(createPinia());
 }
 
 let watchers = [];
